fe089d1f35d97f051583b33620c4b92eeba2da79
Tom Molotnikoff <tdmolotnikoff1@sheffield.ac.uk>
2023-12-05 18:56:27 +0000

    Enable ability to write the first row of the sheet, overwrites each time

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 import os
 from ds18b20_sensor import DS18B20TempSensor
 from dotenv import load_dotenv
+from sheets import Sheets
 
 load_dotenv()
 
 TEMP_SENSOR_NAME = os.getenv("TEMP_SENSOR_NAME")
 TEMP_SENSOR_SHEET_ID = os.getenv("TEMP_SENSOR_SHEET_ID")
+SERVICE_ACCOUNT_KEY_PATH = os.getenv("SERVICE_ACCOUNT_KEY_PATH")
 
 
 def main():
     sensor = DS18B20TempSensor(TEMP_SENSOR_NAME)
+    sheet = Sheets(TEMP_SENSOR_SHEET_ID, SERVICE_ACCOUNT_KEY_PATH)
+    sheet.insert_data(sensor.collect_data(), "sensor_data!A2:C2")
     print(sensor)
 
 
