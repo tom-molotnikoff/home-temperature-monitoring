5f86405d2f7db5115c3b911528bfe7ed095cc5b2
Tom Molotnikoff <tdmolotnikoff1@sheffield.ac.uk>
2023-12-05 18:56:27 +0000

    Enable ability to write the first row of the sheet, overwrites each time

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # temp-sensor-raspberry-pi
 
+# Prerequisites
+1. sudo apt install python3-pip python3.11-venv
+
 # Installation
 
-1. Clone repo
-2. cd temp-sensor-raspberry-pi
-3. pip install -r requirements.txt
+1. cd ~
+2. git clone git@github.com:tom-molotnikoff/temp-sensor-raspberry-pi.git
+3. cd temp-sensor-raspberry-pi
+4. python3 -m venv ./venv
+5. venv/bin/pip3 install -r requirements.txt
+
+Create a .env file and fill with the needed values:\
+TEMP_SENSOR_NAME=name\
+TEMP_SENSOR_SHEET_ID=sheetid\
+SERVICE_ACCOUNT_KEY_PATH=/path/to/google/service/account/key/json
+
+
 
 # Take a reading and ingest to sheet
-1. python3 main.py
+1. ~/temp-sensor-raspberry-pi/venv/bin/python3 main.py
 
 # Updating repo
 1. pip freeze > requirements.txt
---
+++ sheets.py
@@ -0,0 +1,24 @@
+from apiclient import discovery
+from google.oauth2 import service_account
+
+
+class Sheets:
+    sheet_id = ""
+    service = 0
+
+    def __init__(self, sheet_id, secret_file_path):
+        self.sheet_id = sheet_id
+        try:
+            scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file",
+                      "https://www.googleapis.com/auth/spreadsheets"]
+            credentials = service_account.Credentials.from_service_account_file(secret_file_path, scopes=scopes)
+            self.service = discovery.build('sheets', 'v4', credentials=credentials)
+        except OSError as e:
+            print(e)
+
+    def insert_data(self, reading, range_name):
+        """
+        Take a reading from a sensor and input into a Google Sheet at a specified range
+        """
+        self.service.spreadsheets().values().update(spreadsheetId=self.sheet_id, body=reading, range=range_name,
+                                                    valueInputOption='USER_ENTERED').execute()
