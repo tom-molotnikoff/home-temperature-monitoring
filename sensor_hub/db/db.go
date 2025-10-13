2e9a3d485ba2925cec0c6da9d657e4056c8f050d
Tom Molotnikoff <tom.molotnikoff@cirata.com>
2025-10-13 21:19:25 +0100

    Use changesets for the database schema

--- sensor_hub/db/db.go
+++ sensor_hub/db/db.go
@@ -18,7 +18,7 @@ const (
 	TableTemperatureReadings      = "temperature_readings"
 	TableHourlyAverageTemperature = "hourly_avg_temperature"
 )
- 
+
 // This function adds a list of sensor readings to the temperature_readings table in the sensor_database.
 // It expects the readings to be in the form of a slice of SensorReading pointers.
 // Each SensorReading should have a Name and a Reading field, where Reading is a struct containing
@@ -120,96 +120,6 @@ var GetLatestReadings = func() ([]types.APIReading, error) {
 	return apiReadings, nil
 }
 
-// This function creates the temperature_readings table in the sensor_database if it does not exist.
-func createTemperatureReadingsTable() error {
-	query := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
-			id INT AUTO_INCREMENT,
-			sensor_name TEXT NOT NULL,
-			time DATETIME NOT NULL,
-			temperature FLOAT(4) NOT NULL,
-			PRIMARY KEY (id)
-		);`, TableTemperatureReadings)
-
-	_, err := DB.Exec(query)
-	if err != nil {
-		return fmt.Errorf("issue creating temperature readings table: %w", err)
-	}
-	// Create indexes separately, without IF NOT EXISTS
-	_, err = DB.Exec(`CREATE INDEX hourly_idx_time ON temperature_readings (time DESC);`)
-	if err != nil {
-		log.Printf("Could not create hourly_idx_time index (may already exist): %v", err)
-	}
-	_, err = DB.Exec(`CREATE INDEX hourly_idx_sensor_name ON temperature_readings (sensor_name(16));`)
-	if err != nil {
-		log.Printf("Could not create hourly_idx_sensor_name index (may already exist): %v", err)
-	}
-
-	return nil
-}
-
-// This function creates the hourly_average_temperature table and the associated event
-// in the sensor_database if they do not exist. The event calculates the hourly average temperature
-// for each sensor and inserts it into the hourly_average_temperature table every hour.
-// It ensures that duplicate entries for the same sensor and hour are not created.
-func createEventForHourlyAverageTemperature() error {
-	query := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
-			id INT AUTO_INCREMENT,
-			sensor_name VARCHAR(16) NOT NULL,
-			time DATETIME NOT NULL,
-			average_temperature FLOAT(4) NOT NULL,
-			PRIMARY KEY (id),
-			UNIQUE KEY unique_sensor_hour (sensor_name, time)
-		);`, TableHourlyAverageTemperature)
-
-	_, err := DB.Exec(query)
-	if err != nil {
-		return fmt.Errorf("issue creating %s table: %w", TableHourlyAverageTemperature, err)
-	}
-
-	query = fmt.Sprintf(`CREATE INDEX idx_time ON %s (time DESC);`, TableHourlyAverageTemperature)
-	_, err = DB.Exec(query)
-	if err != nil {
-		log.Printf("%s: Could not create idx_time index (may already exist): %v", TableHourlyAverageTemperature, err)
-	}
-	query = fmt.Sprintf(`CREATE INDEX idx_sensor_name ON %s (sensor_name(16));`, TableHourlyAverageTemperature)
-	_, err = DB.Exec(query)
-	if err != nil {
-		log.Printf("%s: Could not create idx_sensor_name index (may already exist): %v", TableHourlyAverageTemperature, err)
-	}
-
-	_, err = DB.Exec("DROP EVENT IF EXISTS hourly_average_temperature_event;")
-	if err != nil {
-		log.Printf("Could not drop existing event (most likely it does not exist): %v", err)
-	}
-
-	query = `
-			CREATE EVENT IF NOT EXISTS hourly_average_temperature_event
-			ON SCHEDULE EVERY 1 HOUR
-			STARTS TIMESTAMP(CURRENT_DATE, SEC_TO_TIME((HOUR(NOW())+1)*3600 + 60))
-			DO
-				INSERT INTO hourly_avg_temperature (sensor_name, time, average_temperature)
-				SELECT
-						tr.sensor_name,
-						DATE_FORMAT(tr.time, '%Y-%m-%d %H:00:00') AS hour,
-						ROUND(AVG(tr.temperature), 2) AS avg_temp
-				FROM temperature_readings tr
-        WHERE tr.time >= DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 HOUR), '%Y-%m-%d %H:00:00')
-          AND tr.time < DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')
-				GROUP BY tr.sensor_name, hour
-				HAVING NOT EXISTS (
-						SELECT 1
-						FROM hourly_avg_temperature hat
-						WHERE hat.sensor_name = tr.sensor_name
-							AND hat.time = hour
-				);
-	`
-	_, err = DB.Exec(query)
-	if err != nil {
-		return fmt.Errorf("issue creating hourly average temperature event: %s", err)
-	}
-	return nil
-}
-
 // This function initialises the database connection and creates the sensor_database if it does not exist.
 // It expects the database properties to be provided through a map with keys:
 // "database.username", "database.password", "database.hostname", and "database.port".
@@ -219,46 +129,14 @@ func InitialiseDatabase() error {
 	db_hostname := appProps.DATABASE_PROPERTIES["database.hostname"]
 	db_port := appProps.DATABASE_PROPERTIES["database.port"]
 
-	jdbc_url := db_username + ":" + db_password + "@(" + db_hostname + ":" + db_port + ")/?parseTime=true"
+	jdbc_url := db_username + ":" + db_password + "@(" + db_hostname + ":" + db_port + ")/sensor_database?parseTime=true"
 
 	db, err := sql.Open("mysql", jdbc_url)
-
-	if err != nil {
-		return fmt.Errorf("could not initialise connection to database: %w", err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return fmt.Errorf("could not ping database: %w", err)
-	}
-
-	create_database_query := "CREATE DATABASE IF NOT EXISTS sensor_database"
-
-	_, err = db.Exec(create_database_query)
-
-	if err != nil {
-		return fmt.Errorf("could not create database: %w", err)
-	}
-
-	db.Close()
-
-	jdbc_url = db_username + ":" + db_password + "@(" + db_hostname + ":" + db_port + ")/sensor_database?parseTime=true"
-
-	db, err = sql.Open("mysql", jdbc_url)
 	if err != nil {
 		return fmt.Errorf("could not initialise connection to database: %w", err)
 	}
 	DB = db
 	log.Println("Connected to database")
-
-	err = createTemperatureReadingsTable()
-	if err != nil {
-		return fmt.Errorf("failed to create temperature readings table: %w", err)
-	}
-	err = createEventForHourlyAverageTemperature()
-	if err != nil {
-		return fmt.Errorf("failed to create hourly average temperature table and event: %w", err)
-	}
 
 	return nil
 }
