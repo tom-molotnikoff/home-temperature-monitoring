fc26454d454aae5123f7b908b241924110f6782f
Tom Molotnikoff <tom.molotnikoff@cirata.com>
2025-12-05 23:35:33 +0000

    Standardise the times on temp readings

--- sensor_hub/utils/utils.go
+++ sensor_hub/utils/utils.go
@@ -4,8 +4,33 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
+
+func NormalizeTimeToSpaceFormat(s string) string {
+	if s == "" {
+		return s
+	}
+	layouts := []string{
+		time.RFC3339Nano,
+		time.RFC3339,
+		"2006-01-02T15:04:05",
+		"2006-01-02 15:04:05",
+		"2006-01-02",
+	}
+	for _, l := range layouts {
+		if t, err := time.Parse(l, s); err == nil {
+			return t.Format("2006-01-02 15:04:05")
+		}
+	}
+
+	if sec, err := strconv.ParseInt(s, 10, 64); err == nil {
+		return time.Unix(sec, 0).Format("2006-01-02 15:04:05")
+	}
+	return s
+}
 
 var ReadPropertiesFile = func(path string) (map[string]string, error) {
 	file, err := os.Open(path)
