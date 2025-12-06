f4df14cec7cd6184851228171426cff854d9cf3b
Tom Molotnikoff <tom.molotnikoff@cirata.com>
2025-12-06 23:14:10 +0000

    Various fixes/efficiency improvements

--- sensor_hub/ui/sensor_hub_ui/src/pages/temperature-dashboard/TemperatureDashboard.tsx
+++ sensor_hub/ui/sensor_hub_ui/src/pages/temperature-dashboard/TemperatureDashboard.tsx
@@ -24,49 +24,57 @@ function TemperatureDashboard() {
   const isMobile = useIsMobile();
 
   return (
-    <DateContextProvider>
-      <PageContainer titleText="Temperature Dashboard">
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            spacing={2}
-            alignItems="stretch"
-            sx={{ minHeight: "100%" }}
-          >
-            {isMobile ? null : (
-              <Grid size={12} sx={{ width: "98vw" }}>
-                <LayoutCard variant="secondary" changes={graphContainerStyle}>
-                  <TypographyH2>Temperature Over Time</TypographyH2>
-                  <DateRangePicker />
-                  <HourlyAveragesToggle
-                    useHourlyAverages={useHourlyAverages}
-                    setUseHourlyAverages={setUseHourlyAverages}
-                  />
-                  <TemperatureGraph
-                    sensors={sensors}
-                    useHourlyAverages={useHourlyAverages}
-                  />
-                  <WeatherChart />
-                </LayoutCard>
+    <PageContainer titleText="Temperature Dashboard">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={2}
+          alignItems="stretch"
+          sx={{ minHeight: "100%" }}
+        >
+          {isMobile ? null : (
+            <>
+              <Grid size={12} sx={{width: "98vw"}}>
+                <DateContextProvider>
+                  <LayoutCard variant="secondary" changes={graphContainerStyle}>
+                    <TypographyH2>Indoor Temperature Data</TypographyH2>
+                    <DateRangePicker/>
+                    <HourlyAveragesToggle
+                      useHourlyAverages={useHourlyAverages}
+                      setUseHourlyAverages={setUseHourlyAverages}/>
+                    <TemperatureGraph
+                      sensors={sensors}
+                      useHourlyAverages={useHourlyAverages}/>
+                  </LayoutCard>
+                </DateContextProvider>
               </Grid>
-            )}
-            <Grid size={isMobile ? 12 : 6}>
-              <SensorsDataGrid
-                sensors={temperatureSensors}
-                cardHeight={"100%"}
-                showReason={false}
-                showType={false}
-                showEnabled={true}
-                title="Temperature Sensors"
-              />
-            </Grid>
-            <Grid size={isMobile ? 12 : 6}>
-              <CurrentTemperatures cardHeight={"100%"} />
-            </Grid>
+              <Grid size={12} sx={{width: "98vw"}}>
+                <DateContextProvider>
+                  <LayoutCard variant="secondary" changes={graphContainerStyle}>
+                    <TypographyH2>Sheffield Weather Data</TypographyH2>
+                    <DateRangePicker/>
+                    <WeatherChart/>
+                  </LayoutCard>
+                </DateContextProvider>
+              </Grid>
+            </>
+          )}
+          <Grid size={isMobile ? 12 : 6}>
+            <SensorsDataGrid
+              sensors={temperatureSensors}
+              cardHeight={"100%"}
+              showReason={false}
+              showType={false}
+              showEnabled={true}
+              title="Temperature Sensors"
+            />
           </Grid>
-        </Box>
-      </PageContainer>
-    </DateContextProvider>
+          <Grid size={isMobile ? 12 : 6}>
+            <CurrentTemperatures cardHeight={"100%"} />
+          </Grid>
+        </Grid>
+      </Box>
+    </PageContainer>
   );
 }
 
